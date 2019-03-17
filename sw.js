2f45415455c259486cfde27cc2f49e0759fd5c5c
Bohdan <ezhichara@gmail.com>
2019-03-18 01:38:14 +0200

    add install pwa

--- sw.js
+++ sw.js
@@ -11,4 +11,25 @@ console.log('Activated', event);
 
 self.addEventListener('push', function(event) {
   console.log('Push message received', event);
-}); 
+});
+
+
+let currentCacheName = 'pwatest-sw-v1';
+
+let arrayOfFilesToCache = [
+    'https://bogdanostrovskiy.github.io/pwa/css/',
+    'https://bogdanostrovskiy.github.io/pwa/js/',
+    'https://bogdanostrovskiy.github.io/pwa/index.html',
+	'https://bogdanostrovskiy.github.io/pwa/features/index.html',
+	'https://bogdanostrovskiy.github.io/pwa/works/index.html',
+    'https://bogdanostrovskiy.github.io/pwa/team/index.html',
+    'https://bogdanostrovskiy.github.io/pwa/contact/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(currentCacheName).then(function(cache) {
+      return cache.addAll(arrayOfFilesToCache);
+    })
+  );
+});
