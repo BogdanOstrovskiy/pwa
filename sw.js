70435ecc1378b14a6cf81d0a5bd5f53bd9fee101
Bogdan Ostrovskiy <bogdan.ostrovskiy@it-devgroup.com>
2019-03-18 09:53:05 +0200

    fix cache bug

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 console.log('Started', self);
 
-self.addEventListener('install', function(event) {
-  // self.skipWaiting();
-  console.log('installed', event);
+self.addEventListener('install', function (event) {
+    // self.skipWaiting();
+    console.log('installed', event);
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     console.log('activated', event);
 });
 
-self.addEventListener('push', function(event) {
+self.addEventListener('push', function (event) {
     console.log('push message received', event);
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     console.log('fetch action received', event);
 
     event.respondWith(
@@ -32,8 +32,8 @@ let arrayOfFilesToCache = [
     '/pwa/css/socicon.css',
     '/pwa/css/style.css',
     '/pwa/index.html',
-	'/pwa/features/index.html',
-	'/pwa/works/index.html',
+    '/pwa/features/index.html',
+    '/pwa/works/index.html',
     '/pwa/team/index.html',
     '/pwa/contact/index.html',
     '/pwa/slick/slick.min.js',
@@ -42,24 +42,23 @@ let arrayOfFilesToCache = [
 ];
 
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-
-    caches.keys().then(function(cacheNames) {
-        return Promise.all(
-            cacheNames.map(function(cacheName) {
-                if (currentCacheName !== cacheName && cacheName.startsWith("pwatest-sw")) {
-                    console.log('cache deleted');
-                    return caches.delete(cacheName);
-                }
-            })
-        );
-    }),
-
-    caches.open(currentCacheName).then(function(cache) {
-
-        console.log(arrayOfFilesToCache , 'Installed', event);
-        return cache.addAll(arrayOfFilesToCache);
-    })
-  );
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (currentCacheName !== cacheName && cacheName.startsWith("pwatest-sw")) {
+                        console.log('cache deleted');
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    event.waitUntil(
+        caches.open(currentCacheName).then(function (cache) {
+            console.log(arrayOfFilesToCache, 'Installed', event);
+            return cache.addAll(arrayOfFilesToCache);
+        })
+    )
 });
